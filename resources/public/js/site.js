a990096ea6a503596cf65a476abe7e51421a85da
Yogthos <dmitri.sotnikov@gmail.com>
2014-03-29 11:11:10 -0400

    fixed animation reloading

--- resources/public/js/site.js
+++ resources/public/js/site.js
@@ -1,21 +1,17 @@
-var totalFrames;
-var frame;
-var t;
 
-function showNextFrame() {
+var animation;
+
+function showNextFrame(totalFrames, frame) {
   $('#frame-' + ((frame > 0) ? frame - 1 : totalFrames)).hide();
   $('#frame-' + frame).show();
-  frame = (frame < totalFrames) ? frame + 1 : 0;
-  t = setTimeout(showNextFrame, 150);
+  setTimeout(function(){showNextFrame(totalFrames, (frame < totalFrames) ? frame + 1 : 0);}, 150);
 }
 
 function render (response) {
-  t = 0;
-  frame = 0;
-  totalFrames = response.frames - 1;
   $("#output").html(response.data);
   $(".loader").hide();
-  showNextFrame();
+  clearTimeout(animation);
+  animation = showNextFrame(response.frames - 1, 0);
 }
 
 function convertImageUrl() {
