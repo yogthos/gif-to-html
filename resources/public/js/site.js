5a59652f183f96b1f7271b985d863230b84f4a89
Yogthos <dmitri.sotnikov@gmail.com>
2014-03-29 11:16:13 -0400

    fixed animation

--- resources/public/js/site.js
+++ resources/public/js/site.js
@@ -4,13 +4,13 @@ var animation;
 function showNextFrame(totalFrames, frame) {
   $('#frame-' + ((frame > 0) ? frame - 1 : totalFrames)).hide();
   $('#frame-' + frame).show();
-  setTimeout(function(){showNextFrame(totalFrames, (frame < totalFrames) ? frame + 1 : 0);}, 150);
+  animation = setTimeout(function(){showNextFrame(totalFrames, (frame < totalFrames) ? frame + 1 : 0);}, 150);
 }
 
 function render (response) {
+  clearTimeout(animation);
   $("#output").html(response.data);
   $(".loader").hide();
-  clearTimeout(animation);
   animation = showNextFrame(response.frames - 1, 0);
 }
 
