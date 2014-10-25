35d17fed5c4d1311b506f02b4670649e7b249ba5
Yogthos <dmitri.sotnikov@gmail.com>
2014-10-25 00:51:05 -0400

    removed unused dependencies

--- project.clj
+++ project.clj
@@ -6,24 +6,16 @@
                  [compojure "1.1.6"]
                  [ring-server "0.3.1"]
                  [selmer "0.7.2"]
-                 [com.taoensso/timbre "3.1.6"]
-                 [environ "1.0.0"]
                  [clj-http "1.0.0"]
                  [net.mikera/imagez "0.4.1"]
                  [bk/ring-gzip "0.1.1"]]
 
-  :repl-options {:init-ns gif-to-html.repl}
-  :plugins [[lein-ring "0.8.13"]
-            [lein-environ "1.0.0"]]
-  :ring {:handler gif-to-html.handler/app
-         :init    gif-to-html.handler/init
-         :destroy gif-to-html.handler/destroy}
+  :plugins [[lein-ring "0.8.13"]]
+  :ring {:handler gif-to-html.handler/app}
   :profiles
   {:uberjar {:aot :all}
    :production {:ring {:open-browser? false
                        :stacktraces?  false
                        :auto-reload?  false}}
-   :dev {:dependencies [[ring-mock "0.1.5"]
-                        [ring/ring-devel "1.3.1"]]
-         :env {:dev true}}}
+   :dev {:dependencies [[ring/ring-devel "1.3.1"]]}}
   :min-lein-version "2.0.0")
--- src/gif_to_html/handler.clj
+++ src/gif_to_html/handler.clj
@@ -1,17 +1,12 @@
 (ns gif-to-html.handler
-  (:require [compojure.core :refer [defroutes]]
+  (:require [compojure.core :refer [GET POST defroutes]]
             [selmer.parser :as parser]
             [clj-http.client :as client]
             [gif-to-html.convert :refer [gif->html]]
             [noir.request :refer [*request*]]
             [noir.response :as response]
             [noir.util.middleware :refer [app-handler]]
-            [compojure.core :refer [GET POST]]
             [compojure.route :as route]
-            [taoensso.timbre :as timbre]
-            [taoensso.timbre.appenders.rotor :as rotor]
-            [selmer.parser :as parser]
-            [environ.core :refer [env]]
             [ring.middleware.defaults :refer [site-defaults]]
             [ring.middleware.gzip :refer [wrap-gzip]]))
 
@@ -27,45 +22,16 @@
    (try
      (gif->html (:body (client/get url {:as :stream})))
      (catch Exception _
-       (.printStackTrace _)
        {:error "The server was displeased with your offering (╯°□°)╯︵ ┻━┻"}))))
 
-(defroutes app-routes
+(defroutes routes
   (GET "/" req (home-page (:query-string req)))
   (POST "/convertImage" [url] (convert-url url))
   (route/resources "/")
   (route/not-found "Not Found"))
 
-(defn init
-  "init will be called once when
-   app is deployed as a servlet on
-   an app server such as Tomcat
-   put any initialization code here"
-  []
-  (timbre/set-config!
-    [:appenders :rotor]
-    {:min-level :info
-     :enabled? true
-     :async? false ; should be always false for rotor
-     :max-message-per-msecs nil
-     :fn rotor/appender-fn})
-
-  (timbre/set-config!
-    [:shared-appender-config :rotor]
-    {:path "gif_to_html_app.log" :max-size (* 512 1024) :backlog 10})
-
-  (if (env :dev) (parser/cache-off!))
-  (timbre/info "gif-to-html-app started successfully"))
-
-(defn destroy
-  "destroy will be called when your application
-   shuts down, put any clean up code here"
-  []
-  (timbre/info "gif-to-html-app is shutting down..."))
-
-(def app (app-handler [app-routes]
-                      :ring-defaults
-                      (-> site-defaults
-                          (assoc-in [:security :xss-protection :enable?] false)
-                          (assoc-in [:security :anti-forgery] false))
-                      :formats [:json-kw]))
+(def app
+  (app-handler
+   [routes]
+   :ring-defaults
+   (assoc-in site-defaults [:security :anti-forgery] false)))
